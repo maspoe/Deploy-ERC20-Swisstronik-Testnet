e5608daeecc2bb975b7c1c0c2228f7d51b378bea
Purwanto . <kontakmaspoe@gmail.com>
2024-07-24 07:16:38 +0000

    first commit

---
+++ contracts/Token.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    constructor()ERC20("DoaIBU","DIBU"){} 
+
+    function mint1000tokens() public {
+        _mint(msg.sender,100000000*10**18);
+    }
+
+    function burn1000tokens() public{
+        _burn(msg.sender,100000000*10**18);
+    }
+    
+}
---
+++ utils/deployed-address.ts
@@ -0,0 +1,3 @@
+const deployedAddress = '0xb84b23c13Cf46A950053c60C318955B0231ae983'
+
+export default deployedAddress
